988311726e8febdae27d0f5b3441965e0988ab73
Metivier <metivier@ipgp.fr>
2026-04-23 21:56:58 +0200

    added simple plot of petite terre

--- examples/Mayotte_Petite_Terre.py
+++ examples/Mayotte_Petite_Terre.py
@@ -20,6 +20,8 @@ import IslandsLib as il
 islands = 'Petite Terre'
 island_fname = "../data/Contours/Indian/Mayotte/Mayotte_Petite_Terre.txt"
 
+
+
 # Name of lake and path to contour
 lake = "Dziani"
 lake_fname = "../data/Contours/Indian/Mayotte/Mayotte_Petite_Terre_dziani.txt"
@@ -53,6 +55,20 @@ fi = 2 * R * 25 / K / 1025
 u, Th, X, Y, Zm, dx, dy, itp = il.IslandLens( islands = islands, fname = island_fname, lakes = lakes, ttype = ttype,  fi = fi , sub_sampling = sub_sampling, clockwise = clockwise, plot = True)
 
 cs = plt.contour(X,Y,Zm, levels=20)
+# dump geojson of contours
 il.dump_geojson(cs,'Mayotte_PT','EPSG:4471')
 
+# Budget of the islands resource
 il.IslandBalance("Petite Terre", Zm, dx, dy, R, K, por)
+
+# plot the island and lake to see
+pt = il.np.loadtxt(island_fname,comments='#',delimiter=',')
+plt.figure()
+plt.plot(pt[:,0],pt[:,1],label='Coast')
+dz = il.np.loadtxt(lake_fname,comments='#',delimiter=',')
+plt.plot(dz[:,0],dz[:,1],label='Dziani lake')
+plt.xlabel('X (m)')
+plt.ylabel('Y (m)')
+plt.axis('equal')
+plt.legend()
+plt.savefig('M_contour.pdf',bbox_inches='tight')
